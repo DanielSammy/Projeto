d7ca980e751bff6e3d2870d7ea3f7c93bbe7c0f0
Daniel Sammy <dandan1702@gmail.com>
2022-05-24 17:39:51 -0300

    .

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>teste</Text>
+      <Text>aewww caraio </Text>
       <StatusBar style="auto" />
     </View>
   );
