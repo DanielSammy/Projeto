fbb90edffd8cc04ffb38a070873d7c1a00e69d77
Otávio Guimarães Silva <70302129+otavioguisilva@users.noreply.github.com>
2022-05-25 15:34:29 -0300

    Criado Navigator e NavigationStack
    Criado Navigator e NavigationStack, alterado para o main ser o src do app.js para não ficar dando erro por ter mudado a pasta do app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
+import React from 'react';
+import registerRootComponent from 'expo/build/launch/registerRootComponent';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator} from '@react-navigation/stack'
 import { View } from 'react-native';
 import { LoginScreen } from './components/Pages/Login';
 import { styles } from './components/Pages/GlobalStyle'; 
 
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={LoginScreen} options={{headerTransparent:true, title:""}} />
+    </Stack.Navigator>
+  )
+}
+
 export default function App() {
   return (
-   
-    <LoginScreen>
-    </LoginScreen>
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
   );
 }
+
+registerRootComponent(App);
 
 
 
